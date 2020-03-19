73aa6c202366092a591d945e81b3a84a0f4d01a7
Aleehcr <42656077+Aleehcr@users.noreply.github.com>
2020-03-19 12:56:00 -0300

    Add Atividade2

---
+++ Atividade2/prjPeso/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace prjPeso
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gpbSexo = new System.Windows.Forms.GroupBox();
+            this.rbMasc = new System.Windows.Forms.RadioButton();
+            this.rbFem = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtAltura = new System.Windows.Forms.MaskedTextBox();
+            this.txtPeso = new System.Windows.Forms.MaskedTextBox();
+            this.lblPesoIdeal = new System.Windows.Forms.Label();
+            this.gpbSexo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gpbSexo
+            // 
+            this.gpbSexo.Controls.Add(this.rbFem);
+            this.gpbSexo.Controls.Add(this.rbMasc);
+            this.gpbSexo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gpbSexo.Location = new System.Drawing.Point(16, 149);
+            this.gpbSexo.Name = "gpbSexo";
+            this.gpbSexo.Size = new System.Drawing.Size(122, 72);
+            this.gpbSexo.TabIndex = 0;
+            this.gpbSexo.TabStop = false;
+            this.gpbSexo.Text = "Sexo";
+            // 
+            // rbMasc
+            // 
+            this.rbMasc.AutoSize = true;
+            this.rbMasc.Checked = true;
+            this.rbMasc.Location = new System.Drawing.Point(7, 20);
+            this.rbMasc.Name = "rbMasc";
+            this.rbMasc.Size = new System.Drawing.Size(98, 24);
+            this.rbMasc.TabIndex = 0;
+            this.rbMasc.TabStop = true;
+            this.rbMasc.Text = "Masculino";
+            this.rbMasc.UseVisualStyleBackColor = true;
+            // 
+            // rbFem
+            // 
+            this.rbFem.AutoSize = true;
+            this.rbFem.Location = new System.Drawing.Point(7, 39);
+            this.rbFem.Name = "rbFem";
+            this.rbFem.Size = new System.Drawing.Size(92, 24);
+            this.rbFem.TabIndex = 1;
+            this.rbFem.Text = "Feminino";
+            this.rbFem.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(105, 9);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(186, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PESO IDEAL";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Insira sua altura:";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(48, 304);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(146, 25);
+            this.lblResultado.TabIndex = 5;
+            this.lblResultado.Text = "RESULTADO:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(49, 334);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(156, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Seu peso ideal seria:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(275, 59);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Insira seu peso:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.White;
+            this.btnCalcular.Location = new System.Drawing.Point(16, 242);
+            this.btnCalcular.Margin = new System.Windows.Forms.Padding(0);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(122, 33);
+            this.btnCalcular.TabIndex = 9;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAltura.Location = new System.Drawing.Point(16, 82);
+            this.txtAltura.Mask = "0.00";
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(122, 26);
+            this.txtAltura.TabIndex = 10;
+            this.txtAltura.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPeso.Location = new System.Drawing.Point(279, 82);
+            this.txtPeso.Mask = "00.00";
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 26);
+            this.txtPeso.TabIndex = 11;
+            this.txtPeso.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblPesoIdeal
+            // 
+            this.lblPesoIdeal.AutoSize = true;
+            this.lblPesoIdeal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPesoIdeal.Location = new System.Drawing.Point(211, 334);
+            this.lblPesoIdeal.Name = "lblPesoIdeal";
+            this.lblPesoIdeal.Size = new System.Drawing.Size(51, 20);
+            this.lblPesoIdeal.TabIndex = 12;
+            this.lblPesoIdeal.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightYellow;
+            this.ClientSize = new System.Drawing.Size(408, 380);
+            this.Controls.Add(this.lblPesoIdeal);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gpbSexo);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Peso Ideal";
+            this.gpbSexo.ResumeLayout(false);
+            this.gpbSexo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gpbSexo;
+        private System.Windows.Forms.RadioButton rbFem;
+        private System.Windows.Forms.RadioButton rbMasc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.MaskedTextBox txtAltura;
+        private System.Windows.Forms.MaskedTextBox txtPeso;
+        private System.Windows.Forms.Label lblPesoIdeal;
+    }
+}
+
