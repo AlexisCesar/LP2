8a3a9ea68115c5e0d8fafc63977c8a0764178ba9
Aleehcr <42656077+Aleehcr@users.noreply.github.com>
2020-03-19 15:02:41 -0300

    Add Atividade3

---
+++ Atividade3/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtA = new System.Windows.Forms.MaskedTextBox();
+            this.txtB = new System.Windows.Forms.MaskedTextBox();
+            this.txtC = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(12, 111);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(70, 20);
+            this.txtA.TabIndex = 0;
+            this.txtA.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtA.ValidatingType = typeof(int);
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(142, 111);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(70, 20);
+            this.txtB.TabIndex = 1;
+            this.txtB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtB.ValidatingType = typeof(int);
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(269, 111);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(70, 20);
+            this.txtC.TabIndex = 2;
+            this.txtC.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtC.ValidatingType = typeof(int);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "A";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(139, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(266, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "C";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(9, 242);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 18);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "TIPO:";
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipo.Location = new System.Drawing.Point(12, 264);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(12, 16);
+            this.lblTipo.TabIndex = 9;
+            this.lblTipo.Text = "-";
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(138, 160);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(78, 29);
+            this.btnVerificar.TabIndex = 10;
+            this.btnVerificar.Text = "VERIFICAR";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(67, 24);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(203, 18);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "ANÁLISE DE TRIÂNGULO";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Honeydew;
+            this.ClientSize = new System.Drawing.Size(351, 354);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.lblTipo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Lados do Triangulo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox txtA;
+        private System.Windows.Forms.MaskedTextBox txtB;
+        private System.Windows.Forms.MaskedTextBox txtC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Atividade3/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double A, B, C;
+            double first, second, third;
+            if (Double.TryParse(txtA.Text, out A) && Double.TryParse(txtB.Text, out B) && Double.TryParse(txtC.Text, out C))
+            {
+                //A as first
+                if (A > B && A > C)
+                {
+                    first = A;
+                    if (B > C)
+                    {
+                        second = B;
+                        third = C;
+                    }
+                    else
+                    {
+                        second = C;
+                        third = B;
+                    }
+                }
+                //B as first
+                else if (B > A && B > C)
+                {
+                    first = B;
+                    if (A > C)
+                    {
+                        second = A;
+                        third = C;
+                    }
+                    else
+                    {
+                        second = C;
+                        third = A;
+                    }
+                }
+                //C as first if(C > B && C > A)
+                else
+                {
+                    first = C;
+                    if (A > B)
+                    {
+                        second = A;
+                        third = B;
+                    }
+                    else
+                    {
+                        second = B;
+                        third = A;
+                    }
+                }
+                /*******************************/
+
+                if (first >= (second + third))
+                {
+                    lblTipo.Text = ("NAO FORMA TRIANGULO");
+                }
+                else if (first == second && first == third)
+                {
+                    lblTipo.Text = ("TRIANGULO EQUILATERO");
+                }
+                else if (first == second || first == third || second == third)
+                {
+                    lblTipo.Text = ("TRIANGULO ISOSCELES");
+                }
+                else
+                {
+                    lblTipo.Text = ("TRIANGULO ESCALENO");
+                }
+                //limpaCampos();
+            }
+            else
+            {
+                MessageBox.Show("Dados inválidos", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                limpaCampos();
+            }
+        }
+
+        private void limpaCampos()
+        {
+            txtA.Text = "";
+            txtB.Text = "";
+            txtC.Text = "";
+        }
+    }
+}
